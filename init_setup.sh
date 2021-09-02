7d734a8be5a7b12f403bdf1025495db4f848b63e
Kensuke Saito <ken.saito.0813@gmail.com>
2021-09-03 08:55:44 +0900

    Added fix to init script and service

--- init_setup.sh
+++ init_setup.sh
@@ -4,6 +4,8 @@ echo "Configuring alarm.conf for you..."
 sed -i "s/username/$USER/" alarm.conf
 
 echo "Configuring alarm.service for you..."
+echo "$PATH" | grep "$HOME/.local/bin" || PATH="$PATH:$HOME/.local/bin"
+sed -i -e "s,path$,$PATH," alarm.service
 sed -i -e "s.homedir.$HOME." -e "s.username.$USER." alarm.service
 sleep 1
 
