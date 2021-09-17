de8ebf115aa556e7dbdcea5d5491d741386ba25e
Kensuke Saito <ken.saito.0813@gmail.com>
2021-09-17 23:05:24 +0900

    Added settings to configure alarm_service.py

--- init_setup.sh
+++ init_setup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/sh
+#!/usr/bin/sh -eux
 
 echo "Configuring alarm.conf for you..."
 sed -i "s/username/$USER/" alarm.conf
@@ -11,7 +11,15 @@ sleep 1
 
 echo "Configuring alarm.service"
 mkdir -p ~/.config/systemd
-cp alarm.service ~/.config/systemd/
+cp alarm.service ~/.config/systemd/user
+sleep 1
+echo 'Done!'
+
+echo "Configuring alarm_service.py"
+mkdir -p ~/.local/bin
+sed -i -e "s.homedir.$HOME." alarm_service.py
+cp alarm_service.py ~/.local/bin
+sleep 1
 echo 'Done!'
 
 echo "Enable the service with systemctl --user enable --now alarm.service"
